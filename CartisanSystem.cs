199c5693718748454fbfe9b2a0fc7289915b1086
Azaruddin8297 <ajarpopo.com>
2023-03-10 12:01:31 +0530

    cartian system

---
+++ CartisanSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProgram
+{
+    public class CartisanSystem
+    {
+        public static void method()
+        {
+            double x1 = 2.0;
+            double y1 = 3.0;
+            double x2 = 4.0;
+            double y2 = 5.0;
+
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of line is: " + length);
+        }
+
+    }
+}
