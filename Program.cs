c9519ece7ede1da2db16cb92e80d61159c9c6d5d
Azaruddin8297 <ajarpopo.com>
2023-03-10 11:49:13 +0530

    Welcom command

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonProgram
+{
+    public class Lineprogram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to the Line Comparison Compution Progarm");
+        }
+    }
+}
